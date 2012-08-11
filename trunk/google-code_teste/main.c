ed0117ad2f3939ec96aecbfb6be4559c89da7903
gustavoluvizotto <gustavoluvizotto@c190bb18-a433-cf30-b9d0-919d544b5d78>
2012-08-11 00:48:46 +0000

    codigo teste

---
+++ trunk/google-code_teste/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: gustavo
+ *
+ * Created on August 10, 2012, 9:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int i = 5;
+    printf("test: %d",i);
+    return (EXIT_SUCCESS);
+}
+
