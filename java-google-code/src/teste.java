c7aeaac450f94859e20fe2dc12e18ac55b91d55c
gustavoluvizotto <gustavoluvizotto@c190bb18-a433-cf30-b9d0-919d544b5d78>
2012-08-11 01:05:00 +0000

    test code using another project. Java project

---
+++ java-google-code/src/teste.java
@@ -0,0 +1,14 @@
+/**
+ * @author gustavo
+ */
+
+/*
+ * Now, I'm doing tests with java.
+ * 
+ */
+public class teste {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("test: " + i);
+    }
+}
