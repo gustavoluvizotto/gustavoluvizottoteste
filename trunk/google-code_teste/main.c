75a100c8b854d83d1b6a87273d1eb17d9c7f5c64
gustavoluvizotto <gustavoluvizotto@c190bb18-a433-cf30-b9d0-919d544b5d78>
2012-08-11 00:53:37 +0000

    test code with some changes.

--- trunk/google-code_teste/main.c
+++ trunk/google-code_teste/main.c
@@ -9,11 +9,18 @@
 #include <stdlib.h>
 
 /*
- * 
+ * making test using google code and NetBeans
+ * all my changes are displayed with a blue 
+ * column at left.  
  */
 int main(int argc, char** argv) {
     int i = 5;
+    
     printf("test: %d",i);
+    
+    for(i = 0; i < 5; i++)
+        printf("%d",i);
+    
     return (EXIT_SUCCESS);
 }
 
